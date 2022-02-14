d4488794c26956599fe0f276b8caca10846d220d
psg107 <psg107@naver.com>
2022-02-14 18:07:56 +0900

    first commit

---
+++ src/main/java/com/example/simplecrud_eureka/SimpleCrudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecrud_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplecrud_eureka/SimpleCrudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecrud_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
