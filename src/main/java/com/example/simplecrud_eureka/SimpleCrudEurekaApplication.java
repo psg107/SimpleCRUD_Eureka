94e70006f48ac2e592fba912537f00bd66c12f2c
psg107 <psg107@naver.com>
2022-02-14 18:25:48 +0900

    Eureka 서버 기본 세팅

--- src/main/java/com/example/simplecrud_eureka/SimpleCrudEurekaApplication.java
+++ src/main/java/com/example/simplecrud_eureka/SimpleCrudEurekaApplication.java
@@ -2,8 +2,10 @@ package com.example.simplecrud_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SimpleCrudEurekaApplication {
 
     public static void main(String[] args) {
